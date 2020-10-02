d9614468256ff59154cb33dd29c23a5030c77264
olie-larner-seeker <olie.larner@seeker.digital>
2020-10-02 12:55:52 +0100

    remove

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,3 @@
-const uri = () => {
-  if (window.location.hostname === "localhost") {
-    return "http://localhost:8888/Setup_Shop/setup-shop-be/graphql"
-  }
-  if (window.location.hostname === "develop.dldoq5hxs3lte.amplifyapp.com") {
-    return "https://wordpress-458085-1446350.cloudwaysapps.com/graphql"
-  }
-}
-
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
